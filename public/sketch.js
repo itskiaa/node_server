eace877523ce854744d115393471dd769f2f64d7
Chiara <37084821+Kiaa1998@users.noreply.github.com>
2020-03-03 14:19:01 +0100

    First Upload

---
+++ public/sketch.js
@@ -0,0 +1,33 @@
+var socket;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(51);
+
+  socket = io.connect("http://localhost:3000") // ** this line creates a socket connecting to the server
+
+  socket.on("mouse", newDrawing); // *** if socket recieves message called "mouse" it triggers function newDrawing()
+}
+
+function newDrawing(data) {
+  fill(255,0,100); // •
+  ellipse(data.x, data.y, 60, 60);
+}
+
+function mouseDragged() {
+  // console.log("Sending:" + mouseX + "," + mouseY);
+
+  var data = { // Create a variable to store the content of the message that's sent to the server --> so that it can be shown on ≠ clients
+    x: mouseX,
+    y: mouseY,  // the content that's sent to the server, and so shared with clients, are mouse coordinates
+                // THIS ALONE IS NOT ENOUGH --> You need to write code on the server.js file so that it can recieve informations from each client
+  }
+
+  socket.emit('mouse', data) // the socket sends to the server the content of var data, as "mouse" informations
+
+  fill(0,0,0); // remind to delare color fill, otherwise after first communication (marked with •) every ellipse is filled with that color --> PROPERTIES ARE OVERWRITTEN
+  ellipse(mouseX, mouseY, 60, 60); // this way you draw only when pressing and moving the mouse
+}
+
+function draw() {
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+// REMINDER --> WHEN YOU WANT TO RUN THIS CODE YOU NEED TO START THE SERVER FROM TERMINAL USING NODE
+
+
+var express = require("express"); // imports Express module as required element in the variable --> used to call function
+
+var app = express(); // express function is used to create an express application
+                     // usign var app you can store the results of the function (which generates the application in a variable for further access)
+
+var server = app.listen(3000); // creates a variable called server where saves the content of var express is recieved on port localhost:3000
+
+app.use(express.static("public")); // creates a line to define which files are dispalyed on server (using folde "public" as source for publicly hosted material)
+                                   // static = hosts static files, files that does not change --> HTML, JavaScript and assets
+
+console.log("Beebo Loves You");
+
+var socket = require("socket.io"); // imports socket.io library as requirement (just like we did for express on line 1)
+                                   // just like express, socket.io is used to call a function
+
+// Time to create an actual socket that is listening to the server on port 3000
+var io =  socket(server); // this variable handles informations coming in and out of the sockets from and to the server
+
+io.sockets.on("connection", newConnection) // what happens on a new connection (JS event trigger) is function newConnection()
+
+function newConnection(socket) { // socket is the function's element --> functions triggers a socket on new connection
+  console.log("new connection: " + socket.id); // for each new connection server logs sockets id (unique id used to track socket)
+ // this function allows you to check when the server recieves a new connection through a log on the console--> in order to work it needs a sending line on the client side meaning public files (will be checked with **)
+
+  socket.on("mouse", mouseMsg) // If a message called "mouse" is recieved, the function mouseMsg() is triggered
+
+  function mouseMsg(data) {
+    console.log(data); // when a message is recived it logs the content of said message (called "data" as in the variable connected to the emitted message from the client)
+    // this code alone does not allow to share messages and data between the clients --> they are stored in to the server, but not shared out
+    // To do so:
+    socket.broadcast.emit("mouse", data); // each time a message called "mouse" containting data, is recieved, it is broadcasted to every socket
+                                          // messages can be altered in servers --> so what comes in appears different once bradcasted i.e. you draw in white, ther client draws in white too, but on your computer is shown in blue
+                                          // to allow clients to recieve messages from other clients trough the server another line of code is needed in the client script (marked ***)
+    // io.socket.emit("mouse", data); // this code sends back the message to every socket connected --> even the one who sent it
+  }
+
+}
